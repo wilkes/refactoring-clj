91abebab17fbfd490618e037cad2296d77cfed78
wilkes <wilkesjoiner@gmail.com>
2016-06-12 10:35:01 -0500

    extract rental-amount

--- src/video_store/customer.clj
+++ src/video_store/customer.clj
@@ -14,21 +14,7 @@
                  :items []
                  :this-amount 0.0})]
     (doseq [rental (-> customer :rentals)]
-      (assoc! state :this-amount 0.0)
-      (condp = (-> rental :movie :price-code)
-        m/REGULAR (do
-                    (assoc! state :this-amount (+ 2 (-> state :this-amount)))
-                    (when (> (-> rental :days-rented) 2)
-                      (assoc! state :this-amount (+ (-> state :this-amount)
-                                                    (* 1.5 (- (-> rental :days-rented) 2))))))
-        m/NEW-RELEASE (do
-                        (assoc! state :this-amount (+ (-> state :this-amount)
-                                                      (* 3 (-> rental :days-rented)))))
-        m/CHILDRENS (do
-                      (assoc! state :this-amount (+ 1.5 (-> state :this-amount)))
-                      (when (> (-> rental :days-rented) 3)
-                        (assoc! state :this-amount (+ (-> state :this-amount)
-                                                      (* 1.5 (- (-> rental :days-rented) 3)))))))
+      (assoc! state :this-amount (r/rental-amount rental))
       (assoc! state :frequent-renter-points (inc (-> state :frequent-renter-points)))
       (when (and (= m/NEW-RELEASE (-> rental :movie :price-code))
                  (> (-> rental :days-rented) 1))
--- src/video_store/rental.clj
+++ src/video_store/rental.clj
@@ -1,3 +1,15 @@
-(ns video-store.rental)
+(ns video-store.rental
+  (:require [video-store.movie :as m]))
 
 (defrecord Rental [movie days-rented])
+
+
+(defn rental-amount [rental]
+  (condp = (-> rental :movie :price-code)
+    m/REGULAR (+ 2.0 (if (> (-> rental :days-rented) 2)
+                       (* 1.5 (- (-> rental :days-rented) 2))
+                       0))
+    m/NEW-RELEASE (* 3.0 (-> rental :days-rented))
+    m/CHILDRENS (+ 1.5 (if (> (-> rental :days-rented) 3)
+                         (* 1.5 (- (-> rental :days-rented) 3))
+                         0))))
