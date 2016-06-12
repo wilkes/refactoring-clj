0afd0e1619b3cc2bbfee5b57ebdece53a1213d57
wilkes <wilkesjoiner@gmail.com>
2016-06-12 10:17:19 -0500

    extracted statement-data

--- src/video_store/customer.clj
+++ src/video_store/customer.clj
@@ -7,6 +7,37 @@
 (defn add-rental [customer rental]
   (update customer :rentals conj rental))
 
+(defn statement-data [customer]
+  (let [state (transient
+                {:total-amount 0.0
+                 :frequent-renter-points 0
+                 :items []
+                 :this-amount 0.0})]
+    (doseq [rental (-> customer :rentals)]
+      (assoc! state :this-amount 0.0)
+      (condp = (-> rental :movie :price-code)
+        m/REGULAR (do
+                    (assoc! state :this-amount (+ 2 (-> state :this-amount)))
+                    (when (> (-> rental :days-rented) 2)
+                      (assoc! state :this-amount (+ (-> state :this-amount)
+                                                    (* 1.5 (- (-> rental :days-rented) 2))))))
+        m/NEW-RELEASE (do
+                        (assoc! state :this-amount (+ (-> state :this-amount)
+                                                      (* 3 (-> rental :days-rented)))))
+        m/CHILDRENS (do
+                      (assoc! state :this-amount (+ 1.5 (-> state :this-amount)))
+                      (when (> (-> rental :days-rented) 3)
+                        (assoc! state :this-amount (+ (-> state :this-amount)
+                                                      (* 1.5 (- (-> rental :days-rented) 3)))))))
+      (assoc! state :frequent-renter-points (inc (-> state :frequent-renter-points)))
+      (when (and (= m/NEW-RELEASE (-> rental :movie :price-code))
+                 (> (-> rental :days-rented) 1))
+        (assoc! state :frequent-renter-points (inc (-> state :frequent-renter-points))))
+      (assoc! state :items (conj (-> state :items) [(-> rental :movie :title)
+                                                    (-> state :this-amount)]))
+      (assoc! state :total-amount (+ (-> state :total-amount) (-> state :this-amount))))
+    (persistent! (dissoc! state :this-amount))))
+
 (defn statement [customer]
   (let [state (transient
                 {:total-amount 0.0
@@ -38,9 +69,9 @@
                                  "\t" (-> state :this-amount) "\n"))
       (assoc! state :total-amount (+ (-> state :total-amount) (-> state :this-amount))))
     (assoc! state :result (str (-> state :result)
-                           "Amount owed is " (-> state :total-amount) "\n"))
+                               "Amount owed is " (-> state :total-amount) "\n"))
     (assoc! state :result (str (-> state :result)
-                           "You earned " (-> state :frequent-renter-points)
+                               "You earned " (-> state :frequent-renter-points)
                                " frequent renter points"))
     (-> state :result)))
 
--- test/video_store/customer_test.clj
+++ test/video_store/customer_test.clj
@@ -53,3 +53,57 @@
             (is (= (c/statement c)
                    "Rental Record for Martin\n\tBambi\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points"))))))))
 
+(deftest statement-test
+  (testing "empty rentals"
+    (is (= (c/statement-data customer) {:frequent-renter-points 0
+                                        :items []
+                                        :total-amount 0.0}))
+    (testing "with one regular rental"
+      (testing "for 1 day"
+        (let [c (c/add-rental customer (r/->Rental regular-movie 1))]
+          (is (= (c/statement-data c) {:frequent-renter-points 1
+                                       :items [["Mad Max" 2.0]]
+                                       :total-amount 2.0}))))
+      (testing "for 2 day"
+        (let [c (c/add-rental customer (r/->Rental regular-movie 2))]
+          (is (= (c/statement-data c) {:frequent-renter-points 1
+                                       :items [["Mad Max" 2.0]]
+                                       :total-amount 2.0}))))
+      (testing "for 3 day"
+        (let [c (c/add-rental customer (r/->Rental regular-movie 3))]
+          (is (= (c/statement-data c) {:frequent-renter-points 1
+                                       :items [["Mad Max" 3.5]]
+                                       :total-amount 3.5}))))
+      (testing "with one new release rental"
+        (testing "for 1 day"
+          (let [c (c/add-rental customer (r/->Rental new-movie 1))]
+            (is (= (c/statement-data c) {:frequent-renter-points 1
+                                         :items [["The Hobbit" 3.0]]
+                                         :total-amount 3.0}))))
+        (testing "for 2 day"
+          (let [c (c/add-rental customer (r/->Rental new-movie 2))]
+            (is (= (c/statement-data c) {:frequent-renter-points 2
+                                         :items [["The Hobbit" 6.0]]
+                                         :total-amount 6.0}))))
+        (testing "for 3 day"
+          (let [c (c/add-rental customer (r/->Rental new-movie 3))]
+            (is (= (c/statement-data c) {:frequent-renter-points 2
+                                         :items [["The Hobbit" 9.0]]
+                                         :total-amount 9.0})))))
+      (testing "with one childrens rental"
+        (testing "for 1 day"
+          (let [c (c/add-rental customer (r/->Rental childrens-movie 1))]
+            (is (= (c/statement-data c) {:frequent-renter-points 1
+                                         :items [["Bambi" 1.5]]
+                                         :total-amount 1.5}))))
+        (testing "for 3 day"
+          (let [c (c/add-rental customer (r/->Rental childrens-movie 3))]
+            (is (= (c/statement-data c) {:frequent-renter-points 1
+                                         :items [["Bambi" 1.5]]
+                                         :total-amount 1.5}))))
+        (testing "for 4 day"
+          (let [c (c/add-rental customer (r/->Rental childrens-movie 4))]
+            (is (= (c/statement-data c) {:frequent-renter-points 1
+                                         :items [["Bambi" 3.0]]
+                                         :total-amount 3.0}))))))))
+
